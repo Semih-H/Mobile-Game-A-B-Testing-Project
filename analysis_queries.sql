59a8dc05fb4dc81aa32e17b74c1ffc142d6f3ea3
Semih <44971794+Semih-H@users.noreply.github.com>
2025-08-15 15:09:12 +0300

    Delete analysis_queries.sql

--- analysis_queries.sql
+++
@@ -1,9 +0,0 @@
--- 1. Missing values check
-
-SELECT 
-    SUM(CASE WHEN cookie_cats.userid IS NULL THEN 1 ELSE 0 END) AS missing_userid,
-    SUM(CASE WHEN cookie_cats.version IS NULL THEN 1 ELSE 0 END) AS missing_version,
-    SUM(CASE WHEN cookie_cats.sum_gamerounds IS NULL THEN 1 ELSE 0 END) AS missing_gamerounds,
-    SUM(CASE WHEN cookie_cats.retention_1 IS NULL THEN 1 ELSE 0 END) AS missing_ret1,
-    SUM(CASE WHEN cookie_cats.retention_7 IS NULL THEN 1 ELSE 0 END) AS missing_ret7
-FROM CookieCats.cookie_cats;
